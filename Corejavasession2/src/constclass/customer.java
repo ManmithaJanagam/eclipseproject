a466436d54c1b6bdcf6e2afd73338c3bfdc5b4cd
Manmitha <janagammanmitha123@gmail.com>
2020-12-18 12:06:17 +0530

    changed the content in sysout to hello

--- Corejavasession2/src/constclass/customer.java
+++ Corejavasession2/src/constclass/customer.java
@@ -48,6 +48,6 @@ public void getDetails()
 	System.out.println("customer PurchaseAmount:"+this.purchaseAmount);
 	System.out.println("Customer details printed");
 	System.out.println("Finished printing");
-	System.out.println("changed");
+	System.out.println("hello");
 }
 }
