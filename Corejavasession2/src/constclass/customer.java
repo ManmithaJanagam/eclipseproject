5e67b9e4a2a7f1a5e01d2b432660cdaf4598e922
Manmitha <janagammanmitha123@gmail.com>
2020-12-18 19:56:29 +0530

    changed the statement to Finished printing customer details in branch

--- Corejavasession2/src/constclass/customer.java
+++ Corejavasession2/src/constclass/customer.java
@@ -47,7 +47,7 @@ public void getDetails()
 	System.out.println("customer gradePoints:"+this.gradePoints);
 	System.out.println("customer PurchaseAmount:"+this.purchaseAmount);
 	System.out.println("Customer details printed");
-	System.out.println("Finished printing the customer details");
+	System.out.println("Finished printing the customer details in branch");
 	System.out.println("welcome");
 	System.out.println("This is a new branch created now");
 	
